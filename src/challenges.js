188666c5b18e77ca8065feb692d2fc40d150fa1a
Daniela Miyasato Goto <daniela.miyasato@live.com>
2021-12-03 20:11:57 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-//O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array. A divisão é feita procurando um padrão, onde o padrão é fornecido como o primeiro parâmetro na chamada do método.
-//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array. A divisão é feita procurando um padrão, onde o padrão é fornecido como o primeiro parâmetro na chamada do método.
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 function splitSentence(phrase) {
   let result = phrase.split(' ');
@@ -37,9 +37,9 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-//A função Math.max() retorna o maior de um ou mais números.
-//O operador spread ( ...) nos permite copiar rapidamente todo ou parte de um array ou objeto existente em outro array ou objeto.
-//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+// A função Math.max() retorna o maior de um ou mais números.
+// O operador spread ( ...) nos permite copiar rapidamente todo ou parte de um array ou objeto existente em outro array ou objeto.
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 function highestCount(valuesArr) {
   let max = Math.max(...valuesArr);
   let result = 0;
@@ -50,12 +50,22 @@ function highestCount(valuesArr) {
   }
   return result;
 }
-let teste = [0, 0, 0];
-console.log(highestCount(teste));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Math.abs(x) returns the absolute (positive) value of x:
+// https://www.w3schools.com/js/js_math.asp
+function catAndMouse(mouse, cat1, cat2) {
+  let gap1 = Math.abs(mouse - cat1);
+  let gap2 = Math.abs(mouse - cat2);
+  let result = '';
+  if (gap1 < gap2) {
+    result = 'cat1';
+  } else if (gap1 > gap2) {
+    result = 'cat2';
+  } else {
+    result = 'os gatos trombam e o rato foge';
+  }
+  return result;
 }
 
 // Desafio 8
